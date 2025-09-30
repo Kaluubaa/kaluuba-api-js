807325f84f9e756d1bec375da487469e4bb3ee68
mananuf <bmananaf@gmail.com>
2025-09-30 18:30:11 +0100

    feat: setup and configures cron job to keep deployment alive

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import userRoutes from "./routes/users.js"
 import clientRoutes from "./routes/clients.js"
 import invoiceRoutes from "./routes/invoices.js"
 import authenticateToken from "./middleware/AuthMiddleware.js";
+import job from "./config/cron.js";
 dotenv.config()
 
 const app = express()
@@ -24,6 +25,7 @@ const port = process.env.PORT || 3030
 const version = process.env.API_VERSION || 1
 const url = `/api/${version}`
 
+if (process.env.NODE_ENV === 'production') job.start();
 
 app.use(`${url}/auth`, authRoutes)
 app.use(`${url}/transactions`, authenticateToken, transactionRoutes)
@@ -33,6 +35,10 @@ app.use(`${url}/invoices`, authenticateToken, invoiceRoutes)
 
 app.get(`/`, (req, res) => {
   res.send('Hello World!')
+})
+
+app.get(`${url}/health`, (req, res) => {
+    res.status(200).json({status: 'OK'});
 })
 
 
